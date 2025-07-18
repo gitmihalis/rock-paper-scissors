fdc8ecfff87a4aa45054be63d0f28fb2898f1c0a
gitmihalis <mihaliswastaken@protonmail.com>
2025-07-18 08:13:49 -0400

    get the computer's choice

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
 console.log("Rock, Paper, Scissors Game");
+
+// 1. Get computer's choice (randomly select between rock, paper, scissors).
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    console.log(computerChoice);
+}
+getComputerChoice()
+// 2. Get user's choice (prompt user for input).
+// 3. Compare choices
+// 4. Determine winner based on the rules:
+// 5. Display the result (who won, or if it's a tie).
