d854a8965610af4ac7503da31fd9bb0035b08e0c
gitmihalis <mihaliswastaken@protonmail.com>
2025-07-18 08:56:15 -0400

    cleanup humanChoice function and round counter

--- rps.js
+++ rps.js
@@ -1,18 +1,19 @@
-console.log("Rock, Paper, Scissors Game");
+console.log("Play Rock, Paper, Scissors!");
 
 // 1. Get computer's choice (randomly select between rock, paper, scissors).
 const choices = ['rock', 'paper', 'scissors'];
+const round = 1;
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
 
-    console.log(choices[computerChoice]);
+    console.log(`computer chose: ${choices[computerChoice]}`);
 }
 
 
 // 2. Get user's choice (prompt user for input).
 function getHumanChoice() {
-    let choice = prompt('Which do you choose? Rock, Paper, or Scissors?');
+    let choice = prompt(`Rock, paper, or scissors? Pick one!`);
     choice = choice.toLowerCase();
     
     console.log(choice);
