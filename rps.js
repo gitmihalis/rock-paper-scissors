ff58cab936959efe5b6368d720c97cb23110c61c
gitmihalis <mihaliswastaken@protonmail.com>
2025-07-18 09:45:18 -0400

    refactor choice functions to return choice
    Better to return the choice so that it can be used elsewhere

--- rps.js
+++ rps.js
@@ -1,28 +1,38 @@
 console.log("Play Rock, Paper, Scissors!");
 
 // 1. Get computer's choice (randomly select between rock, paper, scissors).
-const choices = ['rock', 'paper', 'scissors'];
 const round = 1;
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = Math.floor(Math.random() * 3);
+    console.log(`Computer chose: ${computerChoice}`);
 
-    console.log(`computer chose: ${choices[computerChoice]}`);
+    return choices[computerChoice].toLowerCase();
 }
 
 
 // 2. Get user's choice (prompt user for input).
 function getHumanChoice() {
     let choice = prompt(`Rock, paper, or scissors? Pick one!`);
-    choice = choice.toLowerCase();
-    
-    console.log(choice);
+    console.log(`You chose: ${choice}`);
+
+    return choice.toLowerCase();
 }
 
 // 3. Compare choices
 
 function compareChoices (humanChoice, computerChoice) {
+    switch (humanChoice, computerChoice) {
+        case( humanChoice )       
+    }
     // 4. Determine winner based on the rules:
+    // rock > scissors
+    // scissors > paper
+    // paper > rock
+    // is scissors > rock : no
+    // is paper > scissors : no
+    // is rock > scissors : yes
 }
 
 function playRound () {
